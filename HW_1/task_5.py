788754fc903ec0ec827e750b1747ca152f131108
Dmitriy <ambrose163@mail.ru>
2022-11-28 17:10:50 +0400

    save6

--- HW_1/task_5.py
+++ HW_1/task_5.py
@@ -10,3 +10,10 @@
 
 point_A = input('Введите координаты точки А x,y = ')
 point_B = input('Введите координаты точки В x,y = ')
+xA = int(point_A.split(',')[0])
+yA = int(point_A.split(',')[1])
+xB = int(point_B.split(',')[0])
+yB = int(point_B.split(',')[1])
+
+distance = ((xB - xA) ** 2 + (yB - yA) ** 2) ** 0.5
+print(f'distance = {round(distance, 2)}')
