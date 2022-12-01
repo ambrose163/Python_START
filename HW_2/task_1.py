f288bd1654d1f65b9035197a423f26619d3a5d4b
Dmitriy <ambrose163@mail.ru>
2022-12-01 18:39:07 +0400

    save1

--- HW_2/task_1.py
+++ HW_2/task_1.py
@@ -1,16 +1,9 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
+
 num = input('Введите вещественное число n = ')
 digit_sum = 0
 for i in num:
     if i.isdigit():
         digit_sum += int(i)
 print(f'Сумма цифр числа {num} равна {digit_sum}')
-
-
-# count = 0
-# for i in num:
-#     if i == ".":
-#         i = 0
-#     count += int(i)
-# print(count)9
