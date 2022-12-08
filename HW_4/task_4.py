00f98a204749f67f960b56dbe79c438be5c8e753
Dmitriy <ambrose163@mail.ru>
2022-12-08 18:49:04 +0400

    save4

--- HW_4/task_4.py
+++ HW_4/task_4.py
@@ -1,13 +1,68 @@
-"""
-Даны файлы, в каждом из которых находится запись многочлена.
-Найти сумму многочленов из файлов, ввести в консоль и записать в файл.
-Входными данными для этой задачи являются выходные данные их предыдущей.
+# Даны файлы, в каждом из которых находится запись многочлена.
+# Найти сумму многочленов из файлов, ввести в консоль и записать в файл.
+# Входными данными для этой задачи являются выходные данные их предыдущей.
 
-Ввод: значения типа <str>, полученные из файлов.
-Вывод: значение типа <str>, файл с одной строкой.
 
-Примеры:
-9x^5+7x^4+7x^3+9x^2+6x+17=0
-3x^2+2x+1=0
-9x^5+7x^4+7x^3+12x^2+8x+18=0
-"""
+import re
+
+
+def read_file(name):
+    with open(name, 'r') as f:
+        pol = re.split(' |\*',f.read())
+    return pol
+
+
+def negat_coefs(p):
+    i = 0
+    while p.count('-') != 0:
+        if p[i] == '-':
+            p[i] += p[i + 1]
+            p.pop(i + 1)
+            i = 0
+        i += 1
+    return p
+
+
+p1 = negat_coefs(read_file('pol1.txt'))
+p2 = negat_coefs(read_file('pol2.txt'))
+
+print(p1)
+print(p2)
+
+
+def create_dict(p):
+    dic = {}
+    for i in range(len(p)):
+        if p[i].count('x'):
+            dic[p[i]] = int(p[i - 1])
+        elif p[i] == '=':
+            dic[''] = int(p[i - 1])
+    return(dic)
+
+
+dict_set = (create_dict(p1), create_dict(p2))
+print(dict_set)
+
+# dict_res = {}     """работает только при одинаковом кол-ве элементов"""
+# for key, value in dict1.items():
+#     dict_res[key] = value + dict2[key]
+# print(dict_res)
+
+dict_res = {}
+for diction in dict_set:
+    for key in diction:
+        try:
+            dict_res[key] += diction[key]
+        except KeyError:
+            dict_res[key] = diction[key]
+print(dict_res)
+
+
+str_res = []
+for key, item in dict_res.items():
+    if key == '':
+        str_res.append(f'{item} = 0')
+    else:
+        str_res.append(f'{item}*{key}')
+res = ' + '.join(str_res)
+print(res)
