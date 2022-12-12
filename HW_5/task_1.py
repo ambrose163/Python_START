b96c729c144e3eee8683d86404a1fd96d48d3fbe
Dmitriy <ambrose163@mail.ru>
2022-12-12 15:49:37 +0400

    save1

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -1,7 +1,7 @@
 # Напишите программу, удаляющую из текста все слова, в которых присутствуют буквы «а», «б» и «в».
 
 
-with open('file_task_1.txt', encoding='utf-8') as file: # открыт для чтения
+with open('file_task#1.txt', encoding='utf-8') as file: # открыт для чтения
        data = file.readline()
 
 print(data)
