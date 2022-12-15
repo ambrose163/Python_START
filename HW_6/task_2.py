884eede0ffaa374fd1e2aa0863c1258016520605
Dmitriy <ambrose163@mail.ru>
2022-12-15 15:15:36 +0400

    save2

--- HW_6/task_2.py
+++ HW_6/task_2.py
@@ -1,15 +1,19 @@
-"""
-Задайте список случайных чисел. Выведите:
-а) список чисел, которые не повторяются в заданном списке,
-б) список повторяемых чисел,
-в) список без повторений
+# Задайте список случайных чисел. Выведите:
+# а) список чисел, которые не повторяются в заданном списке,
+# б) список повторяемых чисел,
+# в) список без повторений
 
-Ввод: значение типа <list>
-Вывод: три объекта типа <list>
 
-Пример:
-[1, 2, 3, 5, 1, 5, 3, 10]
-[2, 10]
-[1, 3, 5]
-[1, 2, 5, 3, 10]
-"""
+sequence = '416763761237651623564756376259834'
+
+control_string1 = set()
+control_string2 = set()
+for element in sequence:
+    if element in control_string1:
+        control_string2.add(element)
+    else:
+        control_string1.add(element)
+
+result_sequnce = [element for element in sequence if element not in control_string2]
+
+print(result_sequnce)
